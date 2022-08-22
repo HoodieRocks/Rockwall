a51547e39b09da80b1f7152dcf4260a2455bb03a
boop-the-dog <aidenhperry@gmail.com>
2022-08-22 11:31:38 -0400

    whoops how did that slip through

--- src/main/java/me/cobble/rockwall/cmds/chat/EmojisCommand.kt
+++ src/main/java/me/cobble/rockwall/cmds/chat/EmojisCommand.kt
@@ -1,6 +1,7 @@
 package me.cobble.rockwall.cmds.chat
 
 import me.cobble.rockwall.utils.Formats
+import me.cobble.rockwall.utils.parties.parties.Features
 import org.bukkit.command.CommandSender
 import org.bukkit.command.defaults.BukkitCommand
 import org.bukkit.entity.Player
@@ -10,11 +11,11 @@ class EmojisCommand : BukkitCommand("emojis", "Shows all the available emojis fo
     override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
         if (sender is Player) {
             sender.sendMessage(Formats.color("&e&lEmojis"))
-            for (emoji in Emojis.getAllEmojis()) {
+            for (emoji in Features.getAllEmojis()) {
                 sender.sendMessage(Formats.color("&7${
                     (emoji as String)
                         .replaceFirstChar { it.uppercaseChar() }
-                }: ${Emojis.getEmoji(emoji)}"
+                }: ${Features.getEmoji(emoji)}"
                 )
                 )
             }
